5f187f5811aa921e0bb4332ae678ade807aaec9e
Abdulaziz <abdulaziz.abdumannopov@ssd.uz>
2023-04-03 17:10:41 +0500

    First commit

---
+++ app/Http/Controllers/ChatGptIndexController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Chat;
+use Inertia\Inertia;
+use Inertia\Response;
+
+class ChatGptIndexController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(string $id = null): Response
+    {
+        return Inertia::render('Chat/ChatIndex', [
+            'chat' => fn () => $id ? Chat::findOrFail($id) : null,
+            'messages' => Chat::latest()->where('user_id', auth()->id())->get()
+        ]);
+    }
+}
